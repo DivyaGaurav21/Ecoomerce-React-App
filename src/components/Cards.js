fca92d485a71c84cfb113d1cffaef0b14fec8fd5
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2023-01-20 00:40:00 +0530

    add Cards of Every receipy and restorent details

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Cardsdata from '../CardsData'
+import "./Style.css";
+// import { useDispatch } from 'react-redux';
+// import { ADD } from '../redux/actions/action';
 
 const Cards = () => {
-  return (
-    <div>Cards</div>
-  )
+
+    const [data, setData] = useState(Cardsdata);
+    // console.log(data);
+
+
+    // const dispatch = useDispatch();
+
+
+    // const send = (e) => {
+    //     // console.log(e);
+    //     dispatch(ADD(e));
+    // }
+
+    return (
+        <div className='container mt-3'>
+            <div className="row d-flex justify-content-center align-items-center">
+                {
+                    data.map((element, id) => {
+                        return (
+                            <>
+                                <Card style={{ width: '22rem', border: "none" }} className="mx-2 mt-4 card_style">
+                                    <Card.Img variant="top" src={element.imgdata} style={{ height: "16rem" }} className="mt-3" />
+                                    <Card.Body>
+                                        <Card.Title>{element.rname}</Card.Title>
+                                        <Card.Text>
+                                            Price : ₹ {element.price}
+                                        </Card.Text>
+                                        <div className="button_div d-flex justify-content-center">
+                                            <Button variant="primary"
+                                                className='col-lg-12'>Add to Cart</Button>
+                                        </div>
+
+                                    </Card.Body>
+                                </Card>
+                            </>
+                        )
+                    })
+                }
+
+            </div>
+        </div>
+    )
 }
 
 export default Cards
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 //--------Importing BootStrap Nav Component-------------//
 import Container from 'react-bootstrap/Container';
@@ -6,10 +6,13 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 // -----------Import Badge of Material UI-----------------//
 import Badge from '@mui/material/Badge';
+//-----------Import Model------------------------------//
+import MyVerticallyCenteredModal from './Modals';
 
 
 
 const Header = () => {
+    const [modalShow, setModalShow] = useState(false);
   return (
       <>
           <Navbar bg="danger" variant="dark">
@@ -19,7 +22,11 @@ const Header = () => {
                       <Nav.Link href="#home" className='text-light'>Home</Nav.Link>
                   </Nav>
                   <Badge badgeContent={4} color="primary">
-                      <i class="fa-solid fa-cart-shopping text-light" style={{ fontSize: 25, cursor: "pointer" }}></i>
+                      <i class="fa-solid fa-cart-shopping text-light" style={{ fontSize: 25, cursor: "pointer" }} onClick={() => setModalShow(true)}></i>
+                      <MyVerticallyCenteredModal
+                          show={modalShow}
+                          onHide={() => setModalShow(false)}
+                      />
                   </Badge>
               </Container>
           </Navbar>
