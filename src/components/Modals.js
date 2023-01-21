2d3298e76625bc290357a4bf2bb74b971c27c267
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2023-01-22 02:16:40 +0530

    handle inrease item action in cart

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -14,8 +14,13 @@ import { useSelector } from 'react-redux';
 
 // ------------dispath function import ------------------//
 import { useDispatch } from 'react-redux';
-// ----------Import remove Item Reducer--------------//
-import { removeFromCart } from '../store/cartSlice';
+// ----------Import remove Item Reducer Func-------------//
+// -----------Import decreaseItem Reducer Func-------------//
+// -----------Import addCart Reducer Func-------------//
+import { removeFromCart, decreaseCart, addCart } from '../store/cartSlice';
+
+
+
 
 
 function MyVerticallyCenteredModal(props) {
@@ -28,6 +33,14 @@ function MyVerticallyCenteredModal(props) {
     // ----------Function for deleteItem from Cart-----------//
     const removeCartHandler = (element) => {
         dispatch(removeFromCart(element));
+    }
+    // -----------Function for decrease Cart Item-------------//
+    const decreaseCartItemHandle = (element) => {
+        dispatch(decreaseCart(element))
+    }
+    // -----------Function for increase Cart Item-------------//
+    const increaseCartItemHandle= (element) => {
+        dispatch(addCart(element))
     }
 
     return (
@@ -64,13 +77,10 @@ function MyVerticallyCenteredModal(props) {
                                                         
                                                         <p> <strong>Total</strong>  :₹  {element.price * element.qnty} </p>
                                                         <div className='mt-2 d-flex justify-content-between align-items-center' style={{ width: 90, height:30, borderRadius: 40, cursor: "pointer", background: "#ddd", color: "#111" }}>
-                                                             <span style={{ fontSize: 24 }} >−</span>             
+                                                             <span style={{ fontSize: 24 }} onClick = {() => decreaseCartItemHandle(element)}>−</span>             
                                                             <span style={{ fontSize: 24 , color:'red' }}>{element.qnty}</span>
-                                                             <span style={{ fontSize: 24 }}>+</span>
-                                                            {/* <span style={{ fontSize: 24 }} onClick={ele.qnty <= 1 ? () => dlt(ele.id) : () => remove(ele)}>-</span>
-                                                            <span style={{ fontSize: 22 }}>{ele.qnty}</span>
-                                                           <span style={{ fontSize: 24 }} onClick={() => send(ele)}>+</span> */}
-                                                          </div>
+                                                             <span style={{ fontSize: 24 }} onClick = {()=> increaseCartItemHandle(element)}>+</span>
+                                                         </div>
                                                      </td>
                                                      <td style={{ color: "#fff" }}>
                                                          <p><strong>Rating :</strong> <Ratings value="3" /></p>
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -19,10 +19,13 @@ const cartSlice = createSlice({
         addCart: (state, action) => {
             const itemIndex = state.cartItems.findIndex(item => item.id === action.payload.id);
             if (itemIndex >= 0) {
-                toast.info("Already Added in cart!", {
+                state.cartItems[itemIndex] = {
+                    ...state.cartItems[itemIndex],
+                    qnty: state.cartItems[itemIndex].qnty + 1
+                }
+                toast.info("Item increase in cart!", {
                     position: "top-right"
                 })
-                state.cartItems[itemIndex].qnty += 1;
             } else {
                 const tempProduct = { ...action.payload, qnty: 1 }
                 state.cartItems.push(tempProduct)
@@ -40,13 +43,38 @@ const cartSlice = createSlice({
             state.cartItems = filterCartItems;
         },
 
-        
+        //===========reducer fn for derease item in cart====================//
+        decreaseCart: (state, action) => {
+            const itemIndex = state.cartItems.findIndex(
+                (item) => item.id === action.payload.id
+            );
+
+            if (state.cartItems[itemIndex].qnty > 1) {
+                state.cartItems[itemIndex].qnty -= 1;
+
+                toast.info("Decreased product quantity", {
+                    position: "bottom-left",
+                });
+            } else if (state.cartItems[itemIndex].qnty === 1) {
+                const removeCartItem = state.cartItems.filter(
+                    (item) => item.id !== action.payload.id
+                );
+
+                state.cartItems = removeCartItem;
+
+                toast.error("Product removed from cart !", {
+                    position: "bottom-left",
+                });
+            }
+        },
+       
+
 
 
     }
 });
 
 // Action creators are generated for each case reducer function
-export const { addCart , removeFromCart} = cartSlice.actions;
+export const { addCart, removeFromCart, decreaseCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
